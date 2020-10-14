755c20b3d044472d3949e113d39b49991671cb60
vinit <vinits@spotify.com>
2020-10-14 13:58:16 -0400

    updates

---
+++ Week2/story-maker.py
@@ -0,0 +1,19 @@
+# Assignmnet 3
+
+print ("Welcom to PriLand!")
+print("Answer the questions below to play.")
+print("-----------------------------------")
+
+person1 = input("Enter name: ") 
+person2 = input("Enter 2nd name: ")
+typeOfCandy = input("Your favourite candy: ")
+bakingItem = input("Enter baking item of your choice: ")
+hobby = input("Hobby of your choice: ")
+print("-----------------------------------")
+
+story = " Over the rainbow " + person1 + " and " + person2 + " live, who sell " + typeOfCandy + "for a living." \
++ person2 + " is also a great baker and makes amazing " + bakingItem + ". Lastly she love to " + hobby + "."
+
+print("-----------------------------------")
+print (story)
+
