713703380f89e650abe5a328198242d56a825dc1
vinit <vinits@spotify.com>
2020-10-14 00:43:43 -0400

    added part2

---
+++ Week2/Object_type.py
@@ -0,0 +1,14 @@
+#Assignment 2: Create an object and verify it using https://jsonlint.com/
+
+myRoom = {
+	"size": ["Big", "Small"],
+	"lighting": "Bright",
+	"asthetics": "Mordern-Minimalist",
+	"numberOfObjects": 20,
+	"Area": "midtown",
+	"YearsSpent": 1,
+	"LightsOn": True
+}
+
+print(myRoom)
+print(myRoom["asthetics"])
