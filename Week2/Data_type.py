49ad132d267b517c7b17afae8a16e67236dd39cf
vinit <vinits@spotify.com>
2020-10-14 12:26:10 -0400

    Learning very slowly

---
+++ Week2/Data_type.py
@@ -0,0 +1,39 @@
+#Part 1
+
+myBool = True #Note in JS it needs to be in true
+print(myBool)
+
+myString = "Anything in qoutes is a string"
+print(myString)
+
+word1 = "Have"
+word2 = "Fun"
+print (word1 + word2 + " " + myString) # this will print Have Fun Anything in quotes is a string
+
+myInt = 78
+print(myInt)
+
+myFloat = 3.14
+print(myFloat) #because it has decimal points. 
+
+print(myInt + myFloat) # this will print 783.14
+
+myList = [["Water", "Apple", "Banana"],["max", "jenny"], "watch"]
+print myList
+print myList[2] #this will print watch
+print myList[0][2] #this will Banana
+
+myDict = {
+  "fuel_type": "electric", 
+  "model": "S", 
+  "color": "red"
+}
+print myDict
+print myDict["color"]
+
+
+myString = "1"
+myNum = int(myString)
+
+print myNum
+
